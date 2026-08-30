545d1a5cb0314f123750cde85d095375f666e96a
ilyes <ilyesbouchaala21@gmail.com>
2026-08-30 13:47:43 +0200

    navbar

--- platform-ecom/src/navbar.jsx
+++ platform-ecom/src/navbar.jsx
@@ -1,23 +1,44 @@
+
 function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg bg-dark fixed-top">
       <div className="container">
 
-        <a className="navbar-brand text-white" href="#">
+        {/* Logo + MyShop */}
+        <a
+          className="navbar-brand text-white d-flex align-items-center gap-2"
+          href="#"
+        >
+          <img
+            src="/src/assets/img3.png"
+            alt="MyShop Logo"
+            width="35"
+            height="35"
+            style={{ objectFit: "contain" }}
+          />
+
           MyShop
         </a>
 
+
+        {/* Mobile Button */}
         <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbar"
+          aria-controls="navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
+
+        {/* Navbar Links */}
         <div className="collapse navbar-collapse" id="navbar">
-          <ul className="navbar-nav ms-auto">
+
+          <ul className="navbar-nav ms-auto align-items-lg-center">
 
             <li className="nav-item">
               <a className="nav-link text-white" href="#">
@@ -43,7 +64,22 @@ function NavBar() {
               </a>
             </li>
 
+
+            {/* 🛒 Panier */}
+            <li className="nav-item ms-lg-3 mt-2 mt-lg-0">
+
+              <a
+                href="/cart"
+                className="btn btn-light d-flex align-items-center gap-2"
+              >
+                <i className="bi bi-cart3"></i>
+                🛒 Panier
+              </a>
+
+            </li>
+
           </ul>
+
         </div>
 
       </div>
@@ -52,3 +88,4 @@ function NavBar() {
 }
 
 export default NavBar;
+
