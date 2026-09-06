08d512ad1171c3e5988f8aedb3813e1083c5eb02
ilyesbc-dev <ilyesbouchaala21@gmail.com>
2026-09-06 13:45:26 +0200

    Delete mon-projet directory

--- mon-projet/src/bonjour.jsx
+++
@@ -1,5 +0,0 @@
-function Bonjour() {
-    const name = "ilyes";
-    return <h1>Bonjour {name}</h1>
-}
-export default Bonjour;
--- mon-projet/src/bonsoir.jsx
+++
@@ -1,5 +0,0 @@
-function Bonsoir({ name }) {
-    return <h1>Bonsoir {name}</h1>;
-}
-
-export default Bonsoir;
